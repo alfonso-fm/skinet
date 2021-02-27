8113a60f045b0f8382f8167158e72216df0b29bc
Alfonso <alfonso_fm@hotmail.com>
2021-02-26 19:44:00 -0600

    Change To MAC

--- API/Startup.cs
+++ API/Startup.cs
@@ -9,6 +9,7 @@ using AutoMapper;
 using API.Helpers;
 using API.Middleware;
 using API.Extensions;
+using StackExchange.Redis;
 
 namespace API
 {
@@ -25,7 +26,13 @@ namespace API
         {
             services.AddAutoMapper(typeof(MappingProfiles));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-            services.AddDbContext<StoreContext>(x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<StoreContext>(
+                x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")
+            ));
+            services.AddSingleton<ConnectionMultiplexer>(c => {
+                var configuration = ConfigurationOptions.Parse(_config.GetConnectionString("Redis"), true);
+                return ConnectionMultiplexer.Connect(configuration); 
+            });
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();  
             services.AddCors(opt =>{
